ad3b37f81ff54a57a4277dec750544feb4e4050e
Yuvashree S <97950992+yuvashreesuresh@users.noreply.github.com>
2026-03-23 21:45:32 +0530

    Time: 961 ms (29.94%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,14 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+      AND salary > e.salary
+) < 3;
